fdc44fd686b91de7592c807dc890dafa222a9ec2
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-14 03:41:49 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect (languages)
     updated_array = []
     i =0
     while i < languages.length
-      updated_array << yield (languages[i])
+      updated_array << yield(languages[i])
       i += 1
     end
     updated_array
