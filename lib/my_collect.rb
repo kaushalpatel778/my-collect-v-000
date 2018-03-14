8f1cb5c4091aba43da3dabd6a3b8ce0494b8c5e7
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-14 03:32:27 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,7 @@
-def my_collect (collection)
-  i=0
-  Arrnew = []
-  while i < collection.length
-    Arrnew << yield (collection[i])
-    i += 1
-  end
-  Arrnew
+def my_collect (languages)
+
+  
 end
 
 
-my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
-  name.split(" ").first
-  name.upcase
-end
+
