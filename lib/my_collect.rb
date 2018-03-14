885497f5d2a476b9f77ce759312e8c4b9e4d981b
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-14 03:22:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect (collection)
   i=0
   Arrnew = []
   while i < collection.length
-    Arrnew = <<yield (collection[i])
+    Arrnew << yield (collection[i])
     i += 1
   end
   Arrnew
