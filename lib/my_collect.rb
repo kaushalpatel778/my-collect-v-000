a4c20983cf2cda3cf1b67e649eac5fabc7458fcd
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-14 02:47:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,13 @@
 def my_collect (collection)
   i=0
   while i < collection.length
-    yield collection[i] 
+    yield collection[i]
     i += 1
   end
   collection[i]
 end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
